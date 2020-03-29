8242c6fc40fd0f04897affc64530065bf266d6d9
PeterArriaza <peter.arriaza@gmail.com>
2020-03-29 19:05:32 -0400

    Updates

---
+++ precache-manifest.822c266682dd3c7e509b836c5a799dc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622392d2b155ca1851d036d449a89182",
+    "url": "/pixel-paint/index.html"
+  },
+  {
+    "revision": "b84a9b9904fed8968410",
+    "url": "/pixel-paint/static/css/main.e2a92df8.chunk.css"
+  },
+  {
+    "revision": "f142f67a66d4e9194d80",
+    "url": "/pixel-paint/static/js/2.bf7889ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pixel-paint/static/js/2.bf7889ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b84a9b9904fed8968410",
+    "url": "/pixel-paint/static/js/main.375d957e.chunk.js"
+  },
+  {
+    "revision": "09b41526f575c619a5aa",
+    "url": "/pixel-paint/static/js/runtime-main.e88f48b5.js"
+  }
+]);
